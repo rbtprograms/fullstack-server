fb52dddc5157ca67986175d39d2d42b5d31b4cc0
Arthur Jen <arthurljen@gmail.com>
2018-08-31 10:42:35 -0700

    completed all auth tests

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -47,4 +47,42 @@ describe('Auth API', () => {
                 assert.ok(body.token);
             });
     });
+    it('Gives 400 on signup of same email', () => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email exists');
+            });
+    });
+
+    it('Gives 401 on non-existent email', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'bad@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
+    it('Gives 401 on bad password', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
 });
