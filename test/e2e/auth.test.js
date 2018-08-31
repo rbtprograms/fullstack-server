81ce7e4953b35b3308ac1023348338ab5516cea9
Arthur Jen <arthurljen@gmail.com>
2018-08-31 10:41:36 -0700

    completed sign in test

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -6,7 +6,7 @@ describe('Auth API', () => {
     beforeEach(() => dropCollection('users'));
     beforeEach(() => dropCollection('profiles'));
 
-    let token = null;
+    let token, profile;
 
     beforeEach(() => {
         return request
@@ -16,10 +16,35 @@ describe('Auth API', () => {
                 name: 'tester',
                 password: 'abc'
             })
-            .then(({ body }) => token = body.token);
+            .then(({ body }) => {
+                token = body.token;
+                profile = body.profile;
+            });
     });
 
     it('will signup a user', () => {
         assert.ok(token);
+        assert.equal(profile.name, 'tester');
+    });
+
+    it('verifies', () => {
+        return request
+            .get('/api/auth/verify')
+            .set('Authorization', token)
+            .then(({ body }) => {
+                assert.isOk(body.verified);
+            });
+    });
+
+    it('signs in', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(({ body }) => {
+                assert.ok(body.token);
+            });
     });
 });
